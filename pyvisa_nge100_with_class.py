6c9e0e2aa3559dd661c0d46fe6ced375166a10cb
BaC0To <vasil.v.kirov@gmail.com>
2023-09-26 16:52:04 +0300

    basic setup of functions

--- pyvisa_nge100_with_class.py
+++ pyvisa_nge100_with_class.py
@@ -1,5 +1,8 @@
 
 import pyvisa
+#import logging
+
+
 
 
 VENDOR_ID = '0x0AAD' # Rohde & Schwarz VID_0x0AAD
@@ -7,7 +10,7 @@ VENDOR_ID = '0x0AAD' # Rohde & Schwarz VID_0x0AAD
 
 class PowerSupply:
 
-
+    idn = None
     vendor_id: str
     supported_brands: dict = {
         'ROHDE&SCHWARZ': "0x0AAD"
@@ -18,12 +21,12 @@ class PowerSupply:
 
         self.vendor_id = self.supported_brands.get(brand.upper())
     
-    def __delattr__(self, __name: str) -> None:
-        self.power_supply.disconnect()
+    # def __del__(self) -> None:
+    #     self.power_supply_unit.write("SYSTem:LOCal")
+    #     self.power_supply_unit.close()
 
     def discover(self):
-        """
-        Function that finds the VISA resource of a USB connected PSU"""
+        
         rm = pyvisa.ResourceManager()
         instr_list = rm.list_resources()
         
@@ -35,52 +38,58 @@ class PowerSupply:
         self.power_supply_unit = rm.open_resource(resource_string_1)
 
     def identification(self):
-        """
-        Function that returns IDN information
-        return: str
-        """
-        idn = self.power_supply_unit.query('*IDN?')
-        manufacturer, device_type, part_sn_number, fw_ver = idn.split(',')
-        print(
-            "--- PSU details ---\n"
-            f"Manufacturer: {manufacturer}\n"
-            f"Device_Type: {device_type}\n"
-            f"Part_ Number/Serial: {part_sn_number}\n"
-            f"FW_Ver: {fw_ver.strip()}"
-        )
-        return idn
+        
+        if self.idn is None:
+            idn = self.power_supply_unit.query('*IDN?')
+            manufacturer, device_type, part_sn_number, fw_ver = idn.split(',')
+            print(
+                "--- PSU details ---\n"
+                f"Manufacturer: {manufacturer}\n"
+                f"Device_Type: {device_type}\n"
+                f"Part_ Number/Serial: {part_sn_number}\n"
+                f"FW_Ver: {fw_ver.strip()}"
+            )
+            self.idn = idn
+        return self.idn
 
     def reset(self):
-        """
-        Sets the instrument to a defined default status
-        """
+      
         self.power_supply_unit.write('*RST')
-        pass
 
 
     def select_channel(self, channel:int):
         """
         This function defines the numeric selection of a channel
-        param: channel :int
+        
+        Args:
+            channel(int): The number of the channel.
+        
+        Asserts:
+            None.
+            
+        Returns:
+            None.
+        
+        Raises:
+            ValueError: When .... raises ....
+
+        Examples:
+            ....
+            >>> from x import y
+            >>> y()
         """
         self.channel = channel
         self.power_supply_unit.write(f'INST OUT{self.channel}')
-        pass
+        
 
     def set_voltage_current(self, voltage:float, current:float):
-        """
-        This function defines the voltage and current value of the selected channel
-        param: voltage: float, current: float
-        """
+        
         self.voltage = voltage
         self.current = current
         self.power_supply_unit.write(f'APPLY "{self.voltage},{self.current}"')
 
     def set_overvoltage_protection(self, state:bool, type:str, value:float):
-        """
-        This function defines the OVP settings for the previous selected channel
-        params: state: bool, type (MEAS/PROT): str, value: float
-        """
+       
         self.state = state
         self.type = type
         self.value = value
@@ -90,9 +99,11 @@ class PowerSupply:
         
         self.power_supply_unit.write(f'VOLT:PROT:MODE {self.type}')
         self.power_supply_unit.write(f'VOLT:PROT:LEV {self.value}')
+        
 
 
     def set_fuse(self, state:bool, trip_value:int):
+        
         self.state = state
         self.trip_value = trip_value
 
@@ -101,6 +112,7 @@ class PowerSupply:
         self.power_supply_unit.write(f'FUSE:DEL {self.trip_value}')
 
     def enable_channel(self, state:bool, channel:int):
+        
         self.state = state
         self.channel = channel
 
@@ -108,12 +120,17 @@ class PowerSupply:
         self.power_supply_unit.write(f'OUTP {setting}')
 
     def enable_output(self, state:bool):
-
+       
         self.state = state
         
         setting = "ON" if self.state else "OFF"
         self.power_supply_unit.write(f'OUTPut:GENeral {setting}')
+    
+    def set_local(self, state:bool):
 
+        self.state = state
+        if self.state:
+            self.power_supply_unit.write('SYSTem:LOCal')
 
 
 channel = 1
@@ -122,6 +139,7 @@ over_voltage = 25
 over_voltage_type = 'PROT'
 current = 0.2
 fuse_trip_time = 0.15
+
 
 
 psu1 = PowerSupply('ROHDE&SCHWARZ')
@@ -134,6 +152,6 @@ psu1.set_voltage_current(voltage, current)
 psu1.set_overvoltage_protection(True, 'over_voltage_type', over_voltage)
 psu1.set_fuse(True, fuse_trip_time)
 psu1.enable_channel(True,channel)
-psu1.enable_output(True)
-
+#psu1.enable_output(True)
+psu1.set_local(True)
 
